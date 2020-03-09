101faaa999afc3cdbf6832be2ec2d0b40818d65b
jais anasrulloh <jaisanas2@gmail.com>
2020-03-09 23:44:16 +0700

    add is_safe function in python

--- python/nqueen_solution.py
+++ python/nqueen_solution.py
@@ -1,7 +1,28 @@
-def is_safe(mat, r, c):
-    i = 0
+def is_safe(mat, r, c, N):
+    # check if there is another queen in same column
+    i = r
     while i >= 0:
         if mat[i][c] == "Q":
             return False
         i -= 1
+    
+    # check if there is another queen in same diagonal \
+    i = r
+    j = c
+    while i >= 0 and j >= 0:
+        if mat[i][j] == "Q":
+            return False
+        i -= 1
+        j -= 1
+    
+    # check if there is another queen in same diagonal /
+    i = r
+    j = c
+    while i >= 0 and j < N:
+        if mat[i][j] == "Q":
+            return False
+        i -= 1
+        j += 1
+
+    return True
     
